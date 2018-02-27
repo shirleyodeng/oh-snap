2e1f140ab7122169db6cee611cd1ff507d1a5a40
Shirley Deng <shirleeey@live.com>
2018-02-27 21:36:02 +0000

    Confirmation page created

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create, :destroy]
   end
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :show]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
